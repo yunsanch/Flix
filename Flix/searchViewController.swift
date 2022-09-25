66f0057042090bbf0df32ca29e31c2ac33e2b3bd
yunsanch <60577975+yunsanch@users.noreply.github.com>
2022-09-25 10:04:58 -0400

    added search bar
    new change

--- Flix/searchViewController.swift
+++ Flix/searchViewController.swift
@@ -1,20 +1,17 @@
 //
-//  ViewController.swift
+//  searchViewController.swift
 //  Flix
 //
-//  Created by Yunior Sanchez on 9/15/22.
+//  Created by Yunior Sanchez on 9/25/22.
 //
 
 import UIKit
-import AlamofireImage
 
-class MoviesViewController: UIViewController, UITableViewDataSource,UITableViewDelegate,UISearchResultsUpdating {
+class searchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating {
     
     
+    @IBOutlet weak var tableViewSearch: UITableView!
     
-    
-    
-    @IBOutlet weak var tableView: UITableView!
     var movies = [[String:Any]]()
     
     
@@ -22,17 +19,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource,UITableViewD
     var filteredData: [[String:Any]]!
     var searchController = UISearchController()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-    
-//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//        let vc = storyboard.instantiateViewController(withIdentifier: "MoviesViewController")
-        
-    
-        tableView.dataSource = self
-        tableView.delegate = self
+        tableViewSearch.dataSource = self
+        tableViewSearch.delegate = self
     
         filteredData = movies
         
@@ -43,13 +34,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource,UITableViewD
 //        searchController.navigationItem.hidesSearchBarWhenScrolling = true
         
         searchController.searchBar.sizeToFit()
-        tableView.tableHeaderView = searchController.searchBar
+        tableViewSearch.tableHeaderView = searchController.searchBar
         
         definesPresentationContext = true
+
         // Do any additional setup after loading the view.
-    
-        print("Hello")
-        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -61,8 +50,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource,UITableViewD
                     let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                  
                  self.movies = dataDictionary["results"] as! [[String:Any]]
-                 self.tableView.reloadData()
-                 print(dataDictionary)
+                 
 
                     // TODO: Get the array of movies
                     // TODO: Store the movies in a property to use elsewhere
@@ -72,14 +60,34 @@ class MoviesViewController: UIViewController, UITableViewDataSource,UITableViewD
         }
         task.resume()
     }
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        if let searchText = searchController.searchBar.text {
+            filteredData = searchText.isEmpty ? movies : movies.filter{ movieObject in
+//                return movieObject. .rangeOf (searchText, options:  ) != nil
+                print(movieObject)
+                if let title = movieObject["title"] as? String {
+                    return title.contains(searchText)
+                
+                }
+                return false
+            }
+        
+            
+        
+        }
+        tableViewSearch.reloadData()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return filteredData.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
         
         
-        let movie = filteredData[indexPath.row]
+        let movie = filteredData    [indexPath.row]
         let title = movie["title"] as! String
         let synopsis = movie["overview"] as! String
         
@@ -98,48 +106,32 @@ class MoviesViewController: UIViewController, UITableViewDataSource,UITableViewD
 //        cell.posterView.image = poster
         return cell
     }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //Find the selected movies
-        let cell = sender as! UITableViewCell
-        let indexPath = tableView.indexPath(for: cell)!
+        let cell2 = sender as! UITableViewCell
+        let indexPath = tableViewSearch.indexPath(for: cell2)!
         
         let movie = movies[indexPath.row]
         
         
         //pass the seleted movie to the details view
         
-        let detailsViewController = segue.destination as! MovieDetailsViewController
+        let searchViewController = segue.destination as! searchViewController
         
-        detailsViewController.movie = movie
+//        searchViewController.movie = movie
         
-        tableView.deselectRow(at: indexPath, animated: true)
-    
+        tableViewSearch.deselectRow(at: indexPath, animated: true)
     
     }
-    func updateSearchResults(for searchController: UISearchController) {
-        
-        if let searchText = searchController.searchBar.text {
-            filteredData = searchText.isEmpty ? movies : movies.filter{ movieObject in
-//                return movieObject. .rangeOf (searchText, options:  ) != nil
-                print(movieObject)
-                if let title = movieObject["title"] as? String {
-                    return title.contains(searchText)
-                
-                }
-                return false
-            }
-        
-            
-        
-        }
-        tableView.reloadData()
-        
-        
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
-    
-    
+    */
 
 }
-
